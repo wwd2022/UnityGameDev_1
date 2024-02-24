72145f4741ffca20f4922c8da2b11975fe039564
wwd2022 <107018641+wwd2022@users.noreply.github.com>
2024-02-25 04:27:17 +0900

    1-14 넉백 구현

--- Assets/Scripts/Entities/TopDownMovement.cs
+++ Assets/Scripts/Entities/TopDownMovement.cs
@@ -10,6 +10,8 @@ public class TopDownMovement : MonoBehaviour
     private Vector2 _movementDirection = Vector2.zero;
     private Rigidbody2D _rigidbody;
 
+    private Vector2 _knockback = Vector2.zero;
+    private float knockbackDuration = 0.0f;
     private void Awake()
     {
         _controller = GetComponent<TopDownCharacterController>();
@@ -25,6 +27,10 @@ public class TopDownMovement : MonoBehaviour
     private void FixedUpdate()
     {
         ApplyMovment(_movementDirection);
+        if (knockbackDuration > 0.0f)
+        {
+            knockbackDuration -= Time.fixedDeltaTime;
+        }
     }
 
     private void Move(Vector2 direction)
@@ -32,10 +38,19 @@ public class TopDownMovement : MonoBehaviour
         _movementDirection = direction;
     }
 
+    public void ApplyKnockback(Transform other, float power, float duration)
+    {
+        knockbackDuration = duration;
+        _knockback = -(other.position - transform.position).normalized * power;
+    }
+
     private void ApplyMovment(Vector2 direction)
     {
         direction = direction * _stats.CurrentStates.speed;
-
+        if (knockbackDuration > 0.0f)
+        {
+            direction += _knockback;
+        }
         _rigidbody.velocity = direction;
     }
 }
