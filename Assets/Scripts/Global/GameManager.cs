222bfa6059868cac39a0e7fdf44c150140dc3353
wwd2022 <107018641+wwd2022@users.noreply.github.com>
2024-03-07 10:09:45 +0900

    1-18 UI 만들기

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,11 +12,51 @@ public class GameManager : MonoBehaviour
     public Transform Player { get; private set; }
     [SerializeField] private string playerTag = "Player";
 
+
+    private HealthSystem playerHealthSystem;
+
+    [SerializeField] private TextMeshProUGUI waveText;
+    [SerializeField] private Slider hpGaugeSlider;
+    [SerializeField] private GameObject gameOverUI;
+
     private void Awake()
     {
         instance = this;
         // GameObjec 를 통해서 실행하는 find 함수는 하이어라키탭을 전부 탐색하기 때문에 느려서 Awake, Start 같은 함수에서 사용하는게 좋다
+
         Player = GameObject.FindGameObjectWithTag(playerTag).transform;
 
+        playerHealthSystem = Player.GetComponent<HealthSystem>();
+        playerHealthSystem.OnDamage += UpdateHealthUI;
+        playerHealthSystem.OnHeal += UpdateHealthUI;
+        playerHealthSystem.OnDeath += GameOver;
+
+        gameOverUI.SetActive(false);
+
+    }
+
+    private void UpdateHealthUI()
+    {
+        hpGaugeSlider.value = playerHealthSystem.CurrentHealth / playerHealthSystem.MaxHealth;
+    }
+
+    private void GameOver()
+    {
+        gameOverUI.SetActive(true);
+    }
+
+    private void UpdateWaveUI()
+    {
+        // waveText.text = 
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
