c7d6eb9b1782d04ee2710abbe51186d25689021f
wwd2022 <107018641+wwd2022@users.noreply.github.com>
2024-03-08 15:40:51 +0900

    1-20 스탯 계산하기

--- Assets/Scripts/Entities/CharacterStatsHandler.cs
+++ Assets/Scripts/Entities/CharacterStatsHandler.cs
@@ -1,10 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class CharacterStatsHandler : MonoBehaviour
 {
+    // 최소값 제한
+    private const float MinAttackDelay = 0.03f;
+    private const float MinAttackPower = 0.5f;
+    private const float MinAttackSize = 0.4f;
+    private const float MinAttackSpeed = .1f;
+
+    private const float MinSpeed = 0.8f;
+
+    private const int MinMaxHealth = 5;
+
+
     [SerializeField] private CharacterStats baseStats;
     public CharacterStats CurrentStats { get; private set; }
     public List<CharacterStats> statsModifiers = new List<CharacterStats>();
@@ -13,6 +25,19 @@ public class CharacterStatsHandler : MonoBehaviour
     {
         UpdateCharacterStats();
     }
+
+    public void AddStatModifier(CharacterStats statModifier)
+    {
+        statsModifiers.Add(statModifier);
+        UpdateCharacterStats();
+    }
+
+    public void RemoveStatModifier(CharacterStats statModifier)
+    {
+        statsModifiers.Remove(statModifier);
+        UpdateCharacterStats();
+    }
+
 
     private void UpdateCharacterStats()
     {
@@ -23,10 +48,120 @@ public class CharacterStatsHandler : MonoBehaviour
         }
 
         CurrentStats = new CharacterStats { attackSO = attackSO };
-        // TODO
-        CurrentStats.statsChangeType = baseStats.statsChangeType;
-        CurrentStats.maxHealth = baseStats.maxHealth;
-        CurrentStats.speed = baseStats.speed;
+        // // TODO
+        // CurrentStats.statsChangeType = baseStats.statsChangeType;
+        // CurrentStats.maxHealth = baseStats.maxHealth;
+        // CurrentStats.speed = baseStats.speed;
+        UpdateStats((a, b) => b, baseStats);
+        if (CurrentStats.attackSO != null)
+        {
+            CurrentStats.attackSO.target = baseStats.attackSO.target;
+        }
 
+        foreach (CharacterStats modifier in statsModifiers.OrderBy(o => o.statsChangeType))
+        {
+            if (modifier.statsChangeType == StatsChangeType.Override)
+            {
+                UpdateStats((o, o1) => o1, modifier);
+            }
+            else if (modifier.statsChangeType == StatsChangeType.Add)
+            {
+                UpdateStats((o, o1) => o + o1, modifier);
+            }
+            else if (modifier.statsChangeType == StatsChangeType.Multiple)
+            {
+                UpdateStats((o, o1) => o * o1, modifier);
+            }
+        }
+
+        LimitAllStats();
+    }
+
+    // 업데이트 스텟
+    private void UpdateStats(Func<float, float, float> operation, CharacterStats newModifier)
+    {
+        CurrentStats.maxHealth = (int)operation(CurrentStats.maxHealth, newModifier.maxHealth);
+        CurrentStats.speed = operation(CurrentStats.speed, newModifier.speed);
+
+        if (CurrentStats.attackSO == null || newModifier.attackSO == null)
+            return;
+
+        UpdateAttackStats(operation, CurrentStats.attackSO, newModifier.attackSO);
+
+        if (CurrentStats.attackSO.GetType() != newModifier.attackSO.GetType()) // 공격타입이 다르다면 처리하지않음
+        {
+            return;
+        }
+
+        switch (CurrentStats.attackSO)
+        {
+            // 레인지어택인 경우
+            case RangedAttackData _:
+                ApplyRangedStats(operation, newModifier);
+                break;
+        }
+    }
+
+    // 업데이트 공격 스텟
+    private void UpdateAttackStats(Func<float, float, float> operation, AttackSO currentAttack, AttackSO newAttack)
+    {
+        if (currentAttack == null || newAttack == null)
+        {
+            return;
+        }
+
+        currentAttack.delay = operation(currentAttack.delay, newAttack.delay);
+        currentAttack.power = operation(currentAttack.power, newAttack.power);
+        currentAttack.size = operation(currentAttack.size, newAttack.size);
+        currentAttack.speed = operation(currentAttack.speed, newAttack.speed);
+    }
+
+    // 업데이트 레인지 스텟
+    private void ApplyRangedStats(Func<float, float, float> operation, CharacterStats newModifier)
+    {
+        RangedAttackData currentRangedAttacks = (RangedAttackData)CurrentStats.attackSO;
+
+        if (!(newModifier.attackSO is RangedAttackData))
+        {
+            return;
+        }
+
+        RangedAttackData rangedAttacksModifier = (RangedAttackData)newModifier.attackSO;
+        currentRangedAttacks.multipleProjectilesAngel = operation(currentRangedAttacks.multipleProjectilesAngel, rangedAttacksModifier.multipleProjectilesAngel);
+        currentRangedAttacks.spread = operation(currentRangedAttacks.spread, rangedAttacksModifier.spread);
+        currentRangedAttacks.duration = operation(currentRangedAttacks.duration, rangedAttacksModifier.duration);
+        currentRangedAttacks.numberofProjectilesPerShot = Mathf.CeilToInt(operation(currentRangedAttacks.numberofProjectilesPerShot, rangedAttacksModifier.numberofProjectilesPerShot));
+        currentRangedAttacks.projectileColor = UpdateColor(operation, currentRangedAttacks.projectileColor, rangedAttacksModifier.projectileColor);
+    }
+
+    // 업데이트 컬러
+    private Color UpdateColor(Func<float, float, float> operation, Color currentColor, Color newColor)
+    {
+        return new Color(
+            operation(currentColor.r, newColor.r),
+            operation(currentColor.g, newColor.g),
+            operation(currentColor.b, newColor.b),
+            operation(currentColor.a, newColor.a));
+    }
+
+    // 최소값 제한
+    private void LimitStats(ref float stat, float minVal)
+    {
+        stat = Mathf.Max(stat, minVal);
+    }
+
+    private void LimitAllStats()
+    {
+        if (CurrentStats == null || CurrentStats.attackSO == null)
+        {
+            return;
+        }
+
+        LimitStats(ref CurrentStats.attackSO.delay, MinAttackDelay);
+        LimitStats(ref CurrentStats.attackSO.power, MinAttackPower);
+        LimitStats(ref CurrentStats.attackSO.size, MinAttackSize);
+        LimitStats(ref CurrentStats.attackSO.speed, MinAttackSpeed);
+        LimitStats(ref CurrentStats.speed, MinSpeed);
+        CurrentStats.maxHealth = Mathf.Max(CurrentStats.maxHealth, MinMaxHealth);
     }
 }
