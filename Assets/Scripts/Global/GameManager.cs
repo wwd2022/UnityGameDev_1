f974acd654feadbf5fa8b10acc8b52e9ce122092
wwd2022 <107018641+wwd2022@users.noreply.github.com>
2024-03-08 09:43:06 +0900

    1-19 로직 구현하기

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -19,6 +19,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Slider hpGaugeSlider;
     [SerializeField] private GameObject gameOverUI;
 
+    [SerializeField] private int currentWaveIndex = 0;
+    private int currentSpawnCount = 0;
+    private int waveSpawnCount = 0;
+    private int waveSpawnPosCount = 0; // 스폰되는 몹의 개수
+
+    public float spawnInterval = .5f;
+    public List<GameObject> enemyPrefebs = new List<GameObject>();
+
+    [SerializeField] private Transform spawnPositionsRoot;
+    private List<Transform> spawnPostions = new List<Transform>();
     private void Awake()
     {
         instance = this;
@@ -33,6 +43,70 @@ public class GameManager : MonoBehaviour
 
         gameOverUI.SetActive(false);
 
+        // 스폰포지션의 위치를 전부 가져와서 저장한다
+        for (int i = 0; i < spawnPositionsRoot.childCount; i++)
+        {
+            spawnPostions.Add(spawnPositionsRoot.GetChild(i));
+        }
+    }
+
+    private void Start()
+    {
+        // UpgradeStatInit(); // 업그레이드
+        StartCoroutine("StartNextWave"); // 코루틴 시작
+    }
+
+
+    IEnumerator StartNextWave()
+    {
+        while (true)
+        {
+            if (currentSpawnCount == 0)
+            {
+                UpdateWaveUI(); // Wave 표시 갱신
+                yield return new WaitForSeconds(2f); // 2초간 대기
+
+                if (currentWaveIndex % 20 == 0)
+                {
+                    // RandomUpgrade(); // 랜덤 업그레이드
+                }
+
+                if (currentWaveIndex % 10 == 0)
+                {
+                    waveSpawnPosCount = waveSpawnPosCount + 1 > spawnPostions.Count ? waveSpawnPosCount : waveSpawnPosCount + 1;
+                    waveSpawnCount = 0;
+                }
+
+                if (currentWaveIndex % 5 == 0)
+                {
+                    // CreateReward(); // 보상
+                }
+
+                if (currentWaveIndex % 3 == 0)
+                {
+                    waveSpawnCount += 1;
+                }
+
+
+                for (int i = 0; i < waveSpawnPosCount; i++)
+                {
+                    int posIdx = Random.Range(0, spawnPostions.Count);
+                    for (int j = 0; j < waveSpawnCount; j++)
+                    {
+                        int prefabIdx = Random.Range(0, enemyPrefebs.Count); // 무작위 몹 선택
+                        GameObject enemy = Instantiate(enemyPrefebs[prefabIdx], spawnPostions[posIdx].position, Quaternion.identity); // 몹 생성 
+                        enemy.GetComponent<HealthSystem>().OnDeath += OnEnemyDeath; // 몹사망시 카운트 감소
+
+                        currentSpawnCount++; // 몹 카운트 증가
+                        yield return new WaitForSeconds(spawnInterval); // spawnInterval 초간 대기
+                    }
+                }
+
+                currentWaveIndex++;
+            }
+
+            yield return null;
+        }
     }
 
     private void UpdateHealthUI()
@@ -40,14 +114,20 @@ public class GameManager : MonoBehaviour
         hpGaugeSlider.value = playerHealthSystem.CurrentHealth / playerHealthSystem.MaxHealth;
     }
 
+    private void OnEnemyDeath()
+    {
+        currentSpawnCount--;
+    }
+
     private void GameOver()
     {
         gameOverUI.SetActive(true);
+        StopAllCoroutines(); // 오브젝트에 있는 모든 코루틴 멈춤
     }
 
     private void UpdateWaveUI()
     {
-        // waveText.text = 
+        waveText.text = (currentWaveIndex + 1).ToString();
     }
 
     public void RestartGame()
